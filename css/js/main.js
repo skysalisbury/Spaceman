4d51eaa17aa1d7ed538d393b37484d3b49accc8f
skysalisbury <skysalisbury@gmail.com>
2025-04-25 15:18:20 -0400

    Declare state variables

--- css/js/main.js
+++ css/js/main.js
@@ -2,7 +2,9 @@
 
 
   /*----- state variables -----*/
-
+let winner;
+let board;
+let correctBoard;
 
   /*----- cached elements  -----*/
 
@@ -11,3 +13,21 @@
 
 
   /*----- functions -----*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   GAME STATE IDENTIFICATION:
+// let winner; the win condition
+// let board; The board is the board of letters for the word game.
+// let correctBoard; The board that stores the correct letters once they are guessed/input
+// 
